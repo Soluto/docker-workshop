f0a0fc7cf7c2d5a3d3e88de198afa4029a732c0c
yaron <yaron@soluto.com>
2017-05-23 10:42:57 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+client.ping({
+  // ping usually has a 3000ms timeout
+  requestTimeout: 1000
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+client.create({
+  index: 'myindex',
+  type: 'mytype',
+  id: '1',
+  body: {
+    title: 'Test 1',
+    tags: ['y', 'z'],
+    published: true,
+    published_at: '2013-01-01',
+    counter: 1
+  }
+}, function (error, response) {
+  if (error) {
+    console.trace(error);
+  } else {
+    console.log('index created');
+  }
+});
